3192be76e57c7139e7183b3cb2e64d7d8af5b0a5
Carlos Andres Perez Ubeda <descarlos2013@gmail.com>
2023-08-05 14:44:54 -0600

    Eventos

--- src/pages/Eventos.tsx
+++ src/pages/Eventos.tsx
@@ -1,10 +1,27 @@
-import { Title } from "../components/Title"
-
+import { useEffect, useState } from 'react';
+import { Title } from '../components/Title';
 
 export const Eventos = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => response.json())
+      .then((datos) => setData(datos));
+  }, []);
+
   return (
     <>
-      <Title titulo="Bienvenido!" />
+      <Title titulo="Lista de eventos disponibles!" />
+
+      <ul>
+        {data.map((item: any) => (
+          <li className='relative mb-16' key={item.id}  >
+            <h1 className="text-2xl font-bold uppercase text-slate-600">{item.title}</h1>
+            <p className='text-lg italic'>{item.body}</p>
+            <div className='absolute w-1/2 translate-x-1/2 border-b border-rose-500 -bottom-10'></div>
+          </li>
+        ))}
+      </ul>
     </>
-  )
-}
+  );
+};
